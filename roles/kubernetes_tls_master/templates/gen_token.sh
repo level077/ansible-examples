64a3b44f3264cf9c3ee74ad131fdd30da9cc8d35
root <root@centos7dev.node3.rcs>
2017-11-21 16:45:01 +0800

    fix

---
+++ roles/kubernetes_tls_master/templates/gen_token.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /root/ssl
+BOOTSTRAP_TOKEN=$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ')
+
+cat > token.csv <<EOF
+${BOOTSTRAP_TOKEN},kubelet-bootstrap,10001,"system:kubelet-bootstrap"
+EOF
